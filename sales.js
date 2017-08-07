223afcb44fcea65a6fb189d53e3ffb276ef135b1
Zac-L <Zachlichens@gmail.com>
2017-08-07 15:27:39 -0700

    added for loop for cookie data

--- sales.js
+++ sales.js
@@ -4,14 +4,21 @@ var pdxAirport = {
     maxCustomer: 65,
     Avg: 6.3,
    calculate: function () {
-      console.log(this.maxCustomer - this.minCustomer);
-      console.log(this.minCustomer - this.maxCustomer);
       return Math.floor(Math.random() * (this.maxCustomer - this.minCustomer)) + this.minCustomer;
    },
     avgSale: function () {
       return this.calculate() * this.Avg;
+    },
+    cookieData: function () {
+      var tempArr = [];
+      for (i=0; i < 15; i++) {
+tempArr.push(Math.floor(this.avgSale() ) );
     }
+    return tempArr;
+  }
 }
 
 pdxAirport.calculate();
+
+console.log(pdxAirport.cookieData());
 
