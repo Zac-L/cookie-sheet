b59241e07cd7992a89258e58f9c2d667e2bff2fb
Zac-L <Zachlichens@gmail.com>
2017-08-07 22:00:10 -0700

    finally got total cookies to work

--- sales.js
+++ sales.js
@@ -3,6 +3,15 @@ var pdxAirport = {
     minCustomer: 23,
     maxCustomer: 65,
     Avg: 6.3,
+    tempArr: [],
+    totalCookies: 0,
+
+    getTotal: function() {
+        for (var i = 0; i < 15; i++) {
+            this.totalCookies += this.tempArr[i];
+        }
+    },
+
    calculate: function () {
       return Math.floor(Math.random() * (this.maxCustomer - this.minCustomer)) + this.minCustomer;
    },
@@ -12,26 +21,87 @@ var pdxAirport = {
     },
 
     cookieData: function () {
-      var tempArr = [];
       for (i=0; i < 15; i++) {
-tempArr.push(Math.floor(this.avgSale() ) );
+this.tempArr.push(Math.floor(this.avgSale() ) );
     }
-    return tempArr;
+    return this.tempArr;
   },
 
 addToDom: function () {
     openHours = ['6am: ', '7am: ', '8am: ', '9am: ', '10am: ', '11am: ', '12pm: ', '1pm: ', '2pm: ', '3pm: ', '4pm: ', '5pm: ', '6pm: ', '7pm: ', '8pm: ']
     var listAdd = document.getElementById('cookie');
-        for (var i = 0; i < 15; i++) {
-    var newList = document.createElement('li');
-    newList.innerHTML = (openHours[i] + this.cookieData()[i] + ' cookies');
-    listAdd.appendChild(newList);
-        }
+    // var savedData = this.cookieData()[i];
+        for (var i = 0; i <= 15; i++) {
+            if (i < 15) {
+                var newList = document.createElement('li');
+                newList.innerHTML = (openHours[i] + this.cookieData()[i] + ' cookies');
+                listAdd.appendChild(newList);
+            } else {
+                var newList = document.createElement('li');
+                this.getTotal();
+                newList.innerText = ('Total cookies: ' + this.totalCookies);
+                listAdd.appendChild(newList);
+            }
+        } 
   }
 }
 pdxAirport.addToDom();
 
-console.log(Object.keys(pdxAirport));
+// console.log(this.tempArr);
 
-console.log(pdxAirport.cookieData());
+// console.log(totalCookies);
+
+// console.log(Object.keys(pdxAirport));
+
+// console.log(pdxAirport.cookieData());
+
+var pioneerSquare = {
+    Location: 'pioneerSquare',
+    minCustomer: 3,
+    maxCustomer: 24,
+    Avg: 1.2,
+    tempArr: [],
+    totalCookies: 0,
+
+    getTotal: function() {
+        for (var i = 0; i < 15; i++) {
+            this.totalCookies += this.tempArr[i];
+        }
+    },
+
+   calculate: function () {
+      return Math.floor(Math.random() * (this.maxCustomer - this.minCustomer)) + this.minCustomer;
+   },
+
+    avgSale: function () {
+      return this.calculate() * this.Avg;
+    },
+
+    cookieData: function () {
+      for (i=0; i < 15; i++) {
+this.tempArr.push(Math.floor(this.avgSale() ) );
+    }
+    return this.tempArr;
+  },
+
+addToDom: function () {
+    openHours = ['6am: ', '7am: ', '8am: ', '9am: ', '10am: ', '11am: ', '12pm: ', '1pm: ', '2pm: ', '3pm: ', '4pm: ', '5pm: ', '6pm: ', '7pm: ', '8pm: ']
+    var listAdd = document.getElementById('cookie');
+    // var savedData = this.cookieData()[i];
+        for (var i = 0; i <= 15; i++) {
+            if (i < 15) {
+                var newList = document.createElement('li');
+                newList.innerHTML = (openHours[i] + this.cookieData()[i] + ' cookies');
+                listAdd.appendChild(newList);
+            } else {
+                var newList = document.createElement('li');
+                this.getTotal();
+                newList.innerText = ('Total cookies: ' + this.totalCookies);
+                listAdd.appendChild(newList);
+            }
+        } 
+  }
+}
+pioneerSquare.addToDom();
+
 
